c36748dded97233347113397439e3298399582db
lbo <lbo@spheniscida.de>
2012-07-08 11:33:45 +0200

    Added auto termination to benchmark_close

--- benchmark_close.erl
+++ benchmark_close.erl
@@ -3,11 +3,15 @@
 
 -define(WAITBETWEENSPAWN,30).
 
-main([RemoteHost,Page,Procs,Gets]) -> register(logproc,spawn(fun logger/0)),
+main([RemoteHost,Page,Procs,Gets]) ->
+	register(logproc,spawn(fun logger/0)),
+	register(stopper,spawn(?MODULE,stopper,[list_to_integer(atom_to_list(Procs))])),
 	forker(atom_to_list(RemoteHost),atom_to_list(Page),list_to_integer(atom_to_list(Procs)),list_to_integer(atom_to_list(Gets))).
 
 forker(_RemoteHost, _Page, 0, _Gets) -> done;
-forker(RemoteHost,Page, ToDo,Gets) -> spawn(benchmark_close,get_loop,[RemoteHost,Page,Gets]),
+forker(RemoteHost,Page, ToDo,Gets) ->
+	GetPid = spawn(benchmark_close,get_loop,[RemoteHost,Page,Gets]),
+	stopper ! {newproc,GetPid},
 	sleep(?WAITBETWEENSPAWN),
 	forker(RemoteHost,Page,ToDo-1,Gets).
 
@@ -19,6 +23,22 @@ get_loop(RemoteHost,Page,Gets) ->
 	recv_loop(),
 	gen_tcp:close(Socket),
 	get_loop(RemoteHost,Page,Gets-1).
+
+stopper(0) ->
+	io:format("Terminating http-benchmark...~n"),
+	init:stop();
+stopper(N) ->
+	process_flag(trap_exit,true),
+	receive
+		{newproc,Pid} ->
+			io:format("Registered process ~p~n",[Pid]),
+			link(Pid),
+			stopper(N);
+		{'EXIT',Pid,normal} ->
+			io:format("Process ~p exited normally~n",[Pid]),
+			stopper(N-1)
+	end.
+
 
 recv_loop() ->
 	receive
